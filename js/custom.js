4a9a4213e8b74be011936d48829b53dc726595f1
fischjer4 <fischjer4@gmail.com>
2018-07-19 19:38:51 -0700

    Website Content remake

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+(function($){
+
+	/* ---------------------------------------------- /*
+	 * Preloader
+	/* ---------------------------------------------- */
+
+	$(window).load(function() {
+		$('#status').fadeOut();
+		$('#preloader').delay(350).fadeOut('slow');
+	});
+
+})(jQuery);
